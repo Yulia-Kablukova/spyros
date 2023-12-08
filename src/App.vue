d91bc7845a3cac5b3ceacd8249d02bb412b798f1
Yulia Kablukova <y.kablukova@alumni.nsu.ru>
2023-12-08 22:42:50 +0700

    change filepath

--- src/App.vue
+++ src/App.vue
@@ -810,7 +810,7 @@ const handleErrorPopupClose = () => {
 <template>
   <div>
     <header>
-      <img src="images/header.png" alt="" />
+      <img src="../public/images/header.png" alt="" />
     </header>
 
     <div class="content">
@@ -969,7 +969,7 @@ const handleErrorPopupClose = () => {
       <div v-if="errors.isInvalidTask" class="generator__error-popup">
         <div class="generator__error-container">
           <img
-            src="icons/close.png"
+            src="../public/icons/close.png"
             alt=""
             class="generator__close-icon"
             @click="handleErrorPopupClose"
