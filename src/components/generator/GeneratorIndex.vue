527ddf2a177928d88cbdf5b880d21a3846a474db
Yulia Kablukova <y.kablukova@alumni.nsu.ru>
2026-04-01 01:49:01 +0700

    update version

--- src/components/generator/GeneratorIndex.vue
+++ src/components/generator/GeneratorIndex.vue
@@ -78,7 +78,7 @@ const handleBirthdayPopupClose = () => {
 
 <template>
   <div class="generator">
-    <h1 class="generator__heading">Генератор отчетов<span>2.0.6</span></h1>
+    <h1 class="generator__heading">Генератор отчетов<span>2.0.7</span></h1>
 
     <p class="generator__description">
       Инструмент для написания отчетов по результатам тренировки.
